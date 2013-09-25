63f5d03f889a4b903390b10f9a255d2a746dbab8
Ryan Benoit <rdbenoit@me.com>
2013-09-25 14:30:30 -0600

    Rework for pumpwell specific info
    Signed-off-by: Ryan Benoit <rdbenoit@me.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,17 +17,8 @@
 # limitations under the License.
 #
 
-default['haproxy']['incoming_address'] = "0.0.0.0"
-default['haproxy']['incoming_port'] = 80
-default['haproxy']['member_port'] = 8080
 default['haproxy']['app_server_role'] = "webserver"
 default['haproxy']['balance_algorithm'] = "roundrobin"
-default['haproxy']['enable_ssl'] = false
-default['haproxy']['ssl_incoming_address'] = "0.0.0.0"
-default['haproxy']['ssl_incoming_port'] = 443
-default['haproxy']['ssl_member_port'] = 8443
-default['haproxy']['httpchk'] = nil
-default['haproxy']['ssl_httpchk'] = nil
 default['haproxy']['enable_admin'] = true
 default['haproxy']['admin']['address_bind'] = "127.0.0.1"
 default['haproxy']['admin']['port'] = 22002
@@ -36,12 +27,9 @@ default['haproxy']['stats_socket_path'] = "/var/run/haproxy.sock"
 default['haproxy']['pid_file'] = "/var/run/haproxy.pid"
 
 default['haproxy']['defaults_options'] = ["httplog", "dontlognull", "redispatch"]
-default['haproxy']['x_forwarded_for'] = false
 default['haproxy']['defaults_timeouts']['connect'] = "5s"
 default['haproxy']['defaults_timeouts']['client'] = "50s"
 default['haproxy']['defaults_timeouts']['server'] = "50s"
-default['haproxy']['cookie'] = nil
-
 default['haproxy']['user'] = "haproxy"
 default['haproxy']['group'] = "haproxy"
 
